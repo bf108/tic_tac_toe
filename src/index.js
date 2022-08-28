2eda7290076769cf03597486eaa46b714b8ac635
Ben Farrell <ben.farrell@iqvia.com>
2022-08-28 15:04:30 +0100

    function of square and change of player

--- src/index.js
+++ src/index.js
@@ -6,63 +6,50 @@ import './index.css';
 // Pass properties down from one class with props
 // onClick and handleClick can be called anything but convention is to use onEvent and handleEvent
 
-class Square extends React.Component {
+// class Square extends React.Component {
 
-    render() {
-      return (
-        <button 
-            className="square"
-            // Calls the onClick method from parent (Board)
-            onClick={() => this.props.onClick()}>{this.props.value}</button>
-      );
+//     render() {
+//       return (
+//         <button 
+//             className="square"
+//             // Calls the onClick method from parent (Board)
+//             onClick={() => this.props.onClick()}>{this.props.value}</button>
+//       );
+//     }
+//   }
+    function Square(props){
+        return (
+            <button className="square" onClick={props.onClick}>
+            {props.value}
+            </button>
+        )
     }
-  }
   
   class Board extends React.Component {
     constructor(props){
         super(props)
         this.state = {squares: Array(9).fill(null), 
-                    // players: new Array([1,2]),
-                    // current_player: new Array([1,2])
+                        xIsNext: true
                 }}
 
-    // switchPlayer(){
-
-    //     console.log(`The current player is: ${this.state.current_player}`)
-    //     console.log(this.state.players)
-    //     let new_state = this.state.current_player.slice()
-    //     if (new_state.splice(-1) !== 1){
-    //         new_state.push(1)
-    //         console.log(`New state of current player: ${new_state}`)
-    //         this.setState({current_player: new_state})
-    //         } else {
-    //         new_state.push(2)
-    //         console.log(`New state of current player: ${new_state}`)
-    //         this.setState({current_player: new_state})
-    //     }
-    // }
-
-    // selectShape(){
-    //     return (this.state.current_player.splice(-1) == 1 ? 'X' : 'O')
-    // }
+    selectShape(){
+        return (this.state.xIsNext ? 'X' : 'O')
+    }
 
     // This method updates the value of the square in this.state.squares
     handleClick(i){
         // Shallow copy of the array
         const squares = this.state.squares.slice()
         // Modify the cell which has been clicked
-        squares[i] = 'X'
-        // squares[i] = this.selectShape()
+        // squares[i] = 'X'
+        squares[i] = this.selectShape()
         // Update the state
-        this.setState({squares: squares})
-
-        // Change player
-        // this.switchPlayer()
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        })
 
     }
-
-
-
     renderSquare(i) {
       return (
         <Square
