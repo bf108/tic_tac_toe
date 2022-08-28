0008401b1771fb78ab47863b75ad30d71dc04615
Ben Farrell <ben.farrell@iqvia.com>
2022-08-28 15:17:33 +0100

    added logic for winner

--- src/index.js
+++ src/index.js
@@ -58,9 +58,15 @@ import './index.css';
         />
        );
     }
+
+    checkWinner(){
+        const status = calculateWinner(this.state.squares)
+        return status == null ? `Next player: ${this.selectShape()}` : `Winner is ${status}`
+    }
   
     render() {
-      const status = `Next player: ${this.selectShape()}`;
+    //   const status = `Next player: ${this.selectShape()}`;
+      const status = this.checkWinner()
   
       return (
         <div>
@@ -106,3 +112,22 @@ import './index.css';
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
